154d16da94af366408ade1f9ee921b31ec493312
mochen <2827075398@qq.com>
2023-01-25 19:25:15 +0800

    初始化后端项目

---
+++ battle_city/src/main/java/fun/mochen/battle/city/BattleCityApplication.java
@@ -0,0 +1,13 @@
+package fun.mochen.battle.city;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BattleCityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BattleCityApplication.class, args);
+    }
+
+}
---
+++ battle_city/src/test/java/fun/mochen/battle/city/BattleCityApplicationTests.java
@@ -0,0 +1,13 @@
+package fun.mochen.battle.city;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BattleCityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
